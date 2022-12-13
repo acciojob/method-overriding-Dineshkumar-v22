aa3c9c33ddc7d438db37226d6062f42541a9c0c5
Dinesh <dk159219@gmail.com>
2022-12-13 21:19:06 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static class A{
+        public string meth(){
+        return "Invoking method from class A";
+    }
+        }
+
+    public static class B extends A{
+        public string meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[] args)
+    {
+        B b = new B();
+        b.meth.super();
+
+        B b1 = new B();
+        b1.meth();
+    }
+
+
 }
