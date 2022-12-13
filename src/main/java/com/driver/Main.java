42daf0db9d5edf0ec3c06062c901ed6547f36192
Dinesh <dk159219@gmail.com>
2022-12-13 21:36:18 +0530

    latest

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,8 +15,8 @@ public class Main {
     }
     public static void main(String[] args)
     {
-        B b = new B();
-        b.meth().super();
+        A a = new A();
+        a.meth();
 
         B b1 = new B();
         b1.meth();
